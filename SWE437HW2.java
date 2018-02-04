9ad9b2d463c93af6777c7130bb39756848a83605
Behrad Behmardi <behmardibehrad@gmail.com>
2018-02-03 21:14:52 -0500

    Created 'SWE437HW2.java', this will be the main class for our CLI

---
+++ SWE437HW2.java
@@ -0,0 +1,14 @@
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class SWE437HW2{	
+
+	static QuoteList quoteList;													// instance of QuoteList
+	static HashMap<String, QuoteList> savedSearches = new HashMap<>();			// instance of HashMap to save user's searches
+
+	public static void main(String[] args) {
+														// print main menu
+	}
+}

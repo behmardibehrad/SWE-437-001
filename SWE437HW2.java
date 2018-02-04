221dd898c162887a886b40bc42d6a1bff7519956
admin <admin@admins-MacBook-Pro.local>
2018-02-03 21:19:48 -0500

    Create printMenu method

--- SWE437HW2.java
+++ SWE437HW2.java
@@ -9,6 +9,15 @@ public class SWE437HW2{
 	static HashMap<String, QuoteList> savedSearches = new HashMap<>();			// instance of HashMap to save user's searches
 
 	public static void main(String[] args) {
-														// print main menu
+
+		public static void printMenue() {
+			Scanner keyboard = new Scanner(System.in);  							// Creating a new Scanner named keyboard
+			int mainMenuUsersChoice = 0;											// User's Answer
+			System.out.println("Please select from the following options:\n");		// Printing Menu options for user
+			System.out.println("1. Get a random quote\t(press 1)");					// Printing Menu options for user
+			System.out.println("2. Search\t\t(press 2)");							// Printing Menu options for user
+			System.out.println("3. Exit\t\t\t(press 3)");							// Printing Menu options for user
+			
+		}
 	}
 }
